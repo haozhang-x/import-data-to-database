bef13c0c648b80ed27cce4fcf0d27179c53b34c4
zhanghao <zhanghao.0929@qq.com>
2018-06-21 13:37:09 +0800

    some improve

--- src/main/java/cn/cloudx/importdata/tools/item/ImportItemToDataBase.java
+++ src/main/java/cn/cloudx/importdata/tools/item/ImportItemToDataBase.java
@@ -7,7 +7,6 @@ import cn.cloudx.importdata.repository.item.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
 
